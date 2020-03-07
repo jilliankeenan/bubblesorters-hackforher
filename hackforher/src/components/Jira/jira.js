fbf19d57291128e8eeef799a866d5b3df70095aa
Jillian Keenan <jillian.keenan@puppet.com>
2020-03-07 14:16:55 +0000

    merging changes

--- hackforher/src/components/Jira/jira.js
+++ hackforher/src/components/Jira/jira.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Chart from '../Chart';
 import Heading from '../Heading';
+import SecondaryHeading from '../SecondaryHeading';
 import JiraCounts from '../JiraCounts';
 import './jira.scss';
 
 const Jira = ({ stringLink }) => (
 	<div className="jira-container">
 		<div className="donut-chart">
+      <SecondaryHeading>What Repos have you commited to this month?</SecondaryHeading>
 			<Chart />
 		</div>
 		<div>
